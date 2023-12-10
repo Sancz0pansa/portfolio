257c256d3a81d9a2488c423ab4aa2f673d209ea4
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-12-10 12:47:28 +0100

    responsive

--- src/components/Livechat/Livechat.tsx
+++ src/components/Livechat/Livechat.tsx
@@ -132,7 +132,7 @@ function shaking() {
 
   return (<>
   
-  {liveVisible ? <div className={`livechat ${!liveVisible ? 'hidden' : ''}`}>
+  <div className={`livechat ${!liveVisible ? 'hidden' : ''}`}>
   <div className="close-button" onClick={() => setLiveVisible(false)}></div>
   <div className="livechat__header">
     <div className="livechat__header--photo"></div>
@@ -203,8 +203,8 @@ function shaking() {
   </div> 
   
   
-</div> : <div onClick={() => {setShowBubble(false)
-  setLiveVisible(true)}} className={`livechat__floating ${hidden ? 'hidden' : ''}  ${showBubble ? 'show-bubble' : ''}`}><div><svg className="svgicon" viewBox="0 0 64 64" fill="currentColor" width="30" height="34"><path d="M63.113,18.51v-.16C60.323,7.05,44.582,3,31.972,3S3.582,7,.792,18.5a66.22,66.22,0,0,0,0,20.46c1.18,4.74,5.05,8.63,11.36,11.41l-4,5A3.47,3.47,0,0,0,10.882,61a3.39,3.39,0,0,0,1.44-.31L26.862,54c1.79.18,3.49.27,5.07.27,11.04.04,28.41-4.04,31.18-15.43a60.33,60.33,0,0,0,0-20.33Z"></path></svg></div> </div>}</>
+</div>  <div onClick={() => {setShowBubble(false)
+  setLiveVisible(true)}} className={`livechat__floating ${hidden || liveVisible ? 'hidden' : ''}  ${showBubble ? 'show-bubble' : ''}`}><div><svg className="svgicon" viewBox="0 0 64 64" fill="currentColor" width="30" height="34"><path d="M63.113,18.51v-.16C60.323,7.05,44.582,3,31.972,3S3.582,7,.792,18.5a66.22,66.22,0,0,0,0,20.46c1.18,4.74,5.05,8.63,11.36,11.41l-4,5A3.47,3.47,0,0,0,10.882,61a3.39,3.39,0,0,0,1.44-.31L26.862,54c1.79.18,3.49.27,5.07.27,11.04.04,28.41-4.04,31.18-15.43a60.33,60.33,0,0,0,0-20.33Z"></path></svg></div> </div></>
     
 
 
